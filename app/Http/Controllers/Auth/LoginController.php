d6a37300c8f4f3409e81a961f0a72b3a58cab2af
binvin08 <melomelvin0821@gmail.com>
2019-03-11 10:26:37 +0800

    working version

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Socialite;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+     */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Redirect the user to the GitHub authentication page.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    /**
+     * Obtain the user information from GitHub.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function handleProviderCallback($provider)
+    {
+
+        try {
+            $user = Socialite::driver($provider)->user();
+        } catch (Exception $e) {
+            return redirect('/');
+        }
+
+        $authUser = $this->findOrCreateUser($user, $provider);
+
+        Auth::login($authUser);
+
+        return redirect('/');
+    }
+
+    public function findOrCreateUser($provider, $loginWith)
+    {
+        $authUser = User::where('provider_id', $provider->id)
+            ->where('provider', $loginWith)->first();
+
+        if ($authUser) {
+            return $authUser;
+        }
+
+        return User::create([
+            'name' => $provider->name,
+            'email' => $provider->email,
+            'provider' => $loginWith,
+            'provider_id' => $provider->id,
+        ]);
+    }
+
+}
